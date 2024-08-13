2c4aeb898618de9715be8b3449244cb85fce1097
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-08-13 19:01:10 +0530

    completing smart contract

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -4,6 +4,14 @@ require("@matterlabs/hardhat-zksync-solc");
 module.exports = {
   zksolc: {
     version: "1.3.9",
+    defaultNetwork: "polygon",
+    networks: {
+      hardhat: {},
+      polygon: {
+        url: 'https://rpc-amoy.polygon.technology/',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     compilerSource: "binary",
     settings: {
       optimizer: {
